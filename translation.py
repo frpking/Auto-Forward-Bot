4c04fa4759f241bc9cb96feb6a896a0665ef904f
frpking <92860490+frpking@users.noreply.github.com>
2021-11-30 00:20:30 +0600

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,13 @@
 class Translation(object):
 
       # Start text
-      START = "Hi I am a channel auto forward bot clone of <a href='https://t.me/Autoforward_Nsbot'>Channel Automatic NS bot</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @Ns_bot_updates, @Ns_Bot_supporters"             
+      START = "Hi I am a channel auto forward bot clone of <a href='https://t.me/opencloudxmirror'>Channel Automatic Open Cloud X</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @opencloudxmirror"             
 
       #About text
       ABOUT = """
 <b>📝 Language:</b> Python 3
 <b>🧰 Framework:</b> Pyrogram
 <b>👨‍💻 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
-<b>📢 Channel:</b> NS BOT UPDATES
-<b>👥 Group:</b> Ns BOT SUPPORT
+<b>📢 Channel:</b> OpenCloudX update
+<b>👥 Group:</b> OpenCLoud X support
 <b>🌐Source Code:</b> Press Me 🥰"""
